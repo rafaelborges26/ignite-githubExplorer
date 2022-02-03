a8284f54208c3862b0fb759283e8f2422f119fdb
= <rafael.borges2698@gmail.com>
2022-02-03 18:58:08 -0300

    added fast-refresh in webpack for not refresh states in development environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const htmlWebPackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -15,19 +16,28 @@ module.exports = {
         extensions: ['.js', '.jsx'] //aceitará esses arquivos
     },
     devServer: {
-        static: path.resolve(__dirname, 'public')
+        static: path.resolve(__dirname, 'public'),
+        hot: true
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new htmlWebPackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         }) //setando o plugin para dizer qual arquivo é o arquivo principal index.html
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/, //verificar se termina com .jsx
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }
             },
             {
                 test: /\.scss$/, //verificar se termina com .css
