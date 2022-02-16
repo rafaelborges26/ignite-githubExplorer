b273bc8883f800c02e088135a84d7cf0c975cefc
= <rafael.borges2698@gmail.com>
2022-02-16 03:11:24 -0300

    call github api and show repositories of rocketseat

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+
 
 export function RepositoryList() {
 
@@ -8,14 +12,22 @@ export function RepositoryList() {
         link: "http://globoesporte.com"
     }
 
+    const [repositories,setReporitories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json()) //converter resposta pra json
+        .then(data => setReporitories(data))
+    },[])
+
     return(
         <section className="repository-list" >
             <h1>Lista de Repositórios</h1>
 
             <ul>
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
-               <RepositoryItem repository={repository} />
+                {repositories.map(repository => (
+                    <RepositoryItem repository={repository} key={repository.name} />
+                ))}
             </ul>
         </section>
     )
